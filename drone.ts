7eaef16ccab42cf076950df6d280c74af4f566d1
Yamazaki Kohei <51977859+naikasann@users.noreply.github.com>
2021-12-20 01:04:54 +0900

    更新 pxt.json, main.blocks, main.ts, drone.ts

---
+++ drone.ts
@@ -0,0 +1,180 @@
+/**
+ * Custom blocks
+ */
+//% weight=0 color=#fc4c02 icon="\u26a1"
+//% block="ドローン""
+namespace Tello {
+    /**
+     * ドローンと通信するための無線を設定するよ
+     * @param 通信グループの番号 value 1-100, eg: 1
+     */
+    //% block="無線グループの設定を行う %value"
+    //% group="設定"
+    export function radiosetgroup(value: number): void {
+        radio.setGroup(value)
+    }
+
+    /**
+     * ドローンが上昇するよ！
+     * 移動する距離をえらんでね．
+     * @param value 移動距離を設定する(cm) value 20-500, eg: 20
+     */
+    //% block="%value cm上がる"
+    //% group="つかわないかも"
+    export function up(value: number): void {
+        if (flying == 1) {
+            radio.sendString("up=" + value)
+            flying = 1
+        }
+    }
+
+    /**
+     * ドローンが下降するよ！
+     * 移動する距離をえらんでね．
+     * 地面に近すぎるとドローンがおかしくなるよ．気を付けてね
+     * @param value 移動距離を設定する(cm) value 20-500, eg: 20
+     */
+    //% block="%value cm上がる"
+    //% group="つかわないかも"
+    export function down(value: number): void {
+        if (flying == 1) {
+            radio.sendString("down=" + value)
+            flying = 1
+        }
+    }
+
+    /**
+     * ドローンが前に進むよ！
+     * 移動する距離をえらんでね．
+     * @param value 移動距離を設定する(cm) value 20-500, eg: 20
+     */
+    //% block="%value cmまえにすすむ"
+    //% group="初心者向け"
+    export function forward(value: number): void {
+        if (flying == 1) {
+            radio.sendString("forward " + value)
+            flying = 1
+        }
+    }
+
+    /**
+     * ドローンが後ろに下がるよ！
+     * 移動する距離をえらんでね．
+     * @param value 移動距離を設定する(cm) value 20-500, eg: 20
+     */
+    //% block="%value cmうしろにさがる"
+    //% group="初心者向け"
+    export function back(value: number): void {
+        if (flying == 1) {
+            radio.sendString("back=" + value)
+            flying = 1
+        }
+    }
+
+    /**
+     * ドローンが左に進むよ！
+     * 移動する距離をえらんでね．
+     * @param value 移動距離を設定する(cm) value 20-500, eg: 20
+     */
+    //% block="%value cmひだりにすすむ"
+    //% group="初心者向け"
+    export function left(value: number): void {
+        if (flying == 1) {
+            radio.sendString("left=" + value)
+            flying = 1
+        }
+    }
+
+    /**
+     * ドローンが右に進むよ！
+     * 移動する距離をえらんでね．
+     * @param value 移動距離を設定する(cm) value 20-500, eg: 20
+     */
+    //% block="%value cmみぎにすすむ"
+    //% group="初心者向け"
+    export function right(value: number): void {
+        if (flying == 1) {
+            radio.sendString("right=" + value)
+            flying = 1
+        }
+    }
+
+    /**
+     * ドローンが時計回りに回転するよ
+     * 回転角度をえらんでね．．
+     * @param value 時計回りする角度を設定する value 1-360, eg: 90
+     */
+    //% block="%value 度とけいまわりに回る"
+    //% group="初心者向け"
+    export function rotate_cw(value: number): void {
+        if (flying == 1) {
+            radio.sendString("cw=" + value)
+            flying = 1
+        }
+    }
+
+    /**
+     * ドローンが半時計回りに回転するよ
+     * 回転角度をえらんでね．．
+     * @param value 半時計回りする角度を設定する value 1-360, eg: 90
+     */
+    //% block="はんとけいまわり %value"
+    //% group="初心者向け"
+    export function rotate_ccw(value: number): void {
+        if (flying == 1) {
+            radio.sendString("ccw=" + value)
+            flying = 1
+        }
+    }
+
+    /**
+     * 受信機にドローンが飛行を開始する命令を送信する．
+     */
+    //% block="りりくする"
+    //% group="中級者向け"
+    export function takeoff(): void {
+        radio.sendString("takeoff")
+        flying = 1
+    }
+
+    /**
+     * 受信機にドローンが着地をする命令を送信する．
+    */
+    //% block="ちゃくりくする"
+    //% group="中級者向け"
+    export function land(): void {
+        radio.sendString("land")
+        flying = 0
+    }
+
+    /**
+     * ドローンの飛行状態の変数を取得する．
+    */
+    //% block="ドローンが飛んでいる"
+    //% group="中級者向け"
+    export function getflying(): boolean {
+        if (flying == 1) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    /**
+     * コントローラー内の変数を確認して飛行を開始するか，着地を行う．
+     */
+    //% block="りりく/ちゃくりくする"
+    //% group="初心者向け"
+    export function fly_or_land(): void {
+        if (flying == 0) {
+            radio.sendString("takeoff")
+            flying = 1
+        } else if (flying == 1) {
+            radio.sendString("land")
+            flying = 0
+        }
+    }
+    
+}
+
+let flying = 0
--- main.ts
+++ main.ts
@@ -1,3 +1,76 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.TiltRight, function () {
+    Tello.right(50)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.pause(100)
 })
+input.onButtonPressed(Button.A, function () {
+    distanse += -10
+    basic.showString("" + (distanse))
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    Tello.left(50)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    basic.pause(100)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    Tello.back(50)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(100)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    Tello.forward(50)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Tello.getflying()) {
+        Tello.land()
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        Tello.takeoff()
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . # # # .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    distanse += 10
+    basic.showString("" + (distanse))
+})
+let distanse = 0
+Tello.radiosetgroup(101)
+distanse = 30
